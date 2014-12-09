4fd11da3646668c6c7746ef58778ec47c83f2e60
Ryutaro Kurai <ryu.kurai@gmail.com>
2014-12-09 14:43:34 +0900

    impl print command

--- lib/bdd_ruby/bdd.rb
+++ lib/bdd_ruby/bdd.rb
@@ -1,9 +1,10 @@
 require "bdd_ruby/BDD_WRAP"
 class BDD
   class Node
-    attr_reader :node
-    def initialize(node)
+    attr_reader :node, :manager
+    def initialize(node, manager)
       @node = node
+      @manager = manager
     end
 
     # operators
@@ -13,13 +14,13 @@ class BDD
       xor:     ['hat', :^],
     }.each do |name, arg|
       define_method(name) do |other|
-        Node.new(BDD_WRAP.send(arg[0] + '_bdd', self.node, other.node))
+        Node.new(BDD_WRAP.send(arg[0] + '_bdd', self.node, other.node), self.manager)
       end
       alias_method arg[1], name
     end
 
     def negate
-      Node.new(~(self.node))
+      Node.new(~(self.node), self.manager)
     end
     alias ! negate
 
@@ -38,16 +39,24 @@ class BDD
       negated = (literal.node.GetID[0] == 1 ? true : false)
       varID = literal.node.Top
       if negated
-        Node.new(self.node.At0(varID))
+        Node.new(self.node.At0(varID), self.manager)
       else
-        Node.new(self.node.At1(varID))
+        Node.new(self.node.At1(varID), self.manager)
       end
     end
 
-    def _dnf(node, arr)
-      topID = node.Top
-      _dnf(node.At0(varID))
-      _dnf(node.At1(varID))
+    def dnf
+      ret = []
+      @manager._dnf(self.node, [], ret)
+      ret
+    end
+
+    def true?
+      self.node.GetID == BDD_WRAP::Bddtrue
+    end
+
+    def false?
+      self.node.GetID == BDD_WRAP::Bddfalse
     end
   end
 end
@@ -55,10 +64,11 @@ end
 class BDD
   class Literal < Node
     attr_reader :name, :id
-    def initialize(varID, name)
+    def initialize(varID, name, manager)
       @id    = varID
       @name  = name
       @node  = BDD_WRAP.BDDvar(varID)
+      @manager = manager
     end
   end
 end
@@ -67,14 +77,31 @@ class BDD
   def initialize(init_size = 256, max_size = 2 ** 32 - 1)
     BDD_WRAP.BDD_Init(init_size, max_size)
     @name_table = {}
+    @id_table = {}
   end
 
   def literals(*names)
     names.map do |name|
       @name_table[name] ||= Proc.new{
         varID = BDD_WRAP.BDD_NewVar()
-        Literal.new(varID,name)
+        @id_table[varID] = name
+        Literal.new(varID,name, self)
       }.call
     end
+  end
+
+  def _dnf(node, conjs, ret)
+    id = node.GetID
+    if id == BDD_WRAP::Bddtrue
+      ret << conjs
+      return
+    elsif id == BDD_WRAP::Bddfalse
+      return
+    end
+
+    var_id = node.Top
+    _dnf(node.At1(var_id), conjs.dup.push(@id_table[var_id]), ret)
+    negate = ('!' + @id_table[var_id].to_s).to_sym
+    _dnf(node.At0(var_id), conjs.dup.push(negate), ret)
   end
 end
--- lib/bdd_ruby/zdd.rb
+++ lib/bdd_ruby/zdd.rb
@@ -2,44 +2,52 @@ require "bdd_ruby/BDD_WRAP"
 
 class ZDD
   class Node
-    attr_reader :node
-    def initialize(node)
+    attr_reader :node, :manager
+    def initialize(node, manager)
       @node = node
+      @manager = manager
     end
 
     # operators
     {
-      conjoin: ['and', :&],
-      disjoin: ['bar', :|],
-      xor:     ['hat', :^],
+      intersection: ['and', :&],
+      union: ['plus', :+],
+      product: ['times', :*],
+      minus:   ['minus', :|],
     }.each do |name, arg|
       define_method(name) do |other|
-        Node.new(BDD_WRAP.send(arg[0] + '_zbdd', self.node, other.node))
+        Node.new(BDD_WRAP.send(arg[0] + '_zbdd', self.node, other.node), self.manager)
       end
       alias_method arg[1], name
     end
 
     def negate
-      Node.new(~(self.node))
+      Node.new(~(self.node), self.manager)
     end
     alias ! negate
 
     # attributes
     {size: ['Size'], print: ['Print'] }.each do |name, arg|
       define_method(name) do
-        Node.new(self.node.send(arg[0]))
+        Node.new(self.node.send(arg[0]), self.manager)
       end
     end
 
+    def family
+      ret = []
+      @manager._family(self.node, [], ret)
+      ret
+    end
   end
 end
 
 class ZDD
   class Literal < Node
     attr_reader :name
-    def initialize(varID, name)
+    def initialize(var_id, name, manager)
       @name = name
-      @node = BDD_WRAP::ZBDD.new(1).Change(varID)
+      @node = BDD_WRAP::ZBDD.new(1).Change(var_id)
+      @manager = manager
     end
   end
 end
@@ -48,14 +56,30 @@ class ZDD
   def initialize(init_size = 256, max_size = 2 ** 32 - 1)
     BDD_WRAP.BDD_Init(init_size, max_size)
     @name_table = {}
+    @id_table = {}
   end
 
   def literals(*names)
     names.map do |name|
       @name_table[name] ||= Proc.new{
-        varID = BDD_WRAP.BDD_NewVar
-        Literal.new(varID,name)
+        var_id = BDD_WRAP.BDD_NewVar
+        @id_table[var_id] = name
+        Literal.new(var_id,name,self)
       }.call
     end
+  end
+
+  def _family(node, set, ret)
+    id = node.GetID
+    if id == BDD_WRAP::Bddsingle
+      ret << set
+      return
+    elsif id == BDD_WRAP::Bddempty
+      return
+    end
+
+    var_id = node.Top
+    _family(node.OnSet0(var_id), set.dup.push(@id_table[var_id]), ret)
+    _family(node.OffSet(var_id), set.dup, ret)
   end
 end
